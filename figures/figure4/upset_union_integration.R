e5099a3b243aebe304db519102837326f39febb9
Moritz Schaefer <mail@moritzs.de>
2021-10-13 00:35:37 +0200

    beauti-fixes

--- figures/figure4/upset_union_integration.R
+++ figures/figure4/upset_union_integration.R
@@ -1,9 +1,6 @@
-library(ComplexHeatmap)  # TODO add to conda.yaml
+library(ComplexHeatmap)
 library(dplyr)
 library(openxlsx)
-
-
-# TODO table should include gene_ids (not gene_names..)
 
 # we need the whole set of DEGs (1046 vs 745 oder so)
 ago21_specifics <- read.xlsx("../../data/TableS1_RNA-seq.xlsx",
@@ -23,12 +20,9 @@ for (status in c("UP", "DOWN")) {
     filtered <- filter(df, Status == status)
     return(filtered$Gene.name)
   })
-
   mat <- make_comb_mat(lt, mode = "union", universal_set = ago21_filtered$Gene.name)
-
   pdf(paste0("analyses_overlap_", status, "s.pdf"), width=8, height=3)
   UpSet(mat, comb_order = order(comb_size(mat), decreasing=TRUE))
   dev.off()
-
-  # list_to_matrix(lt, universal = ago21_specifics_up)
 }
+
